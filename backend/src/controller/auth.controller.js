71dcddd9753c8644fd47054ce141d238660d2f36
Ankit Niranjan <ankitniranjan331@gmail.com>
2025-09-06 14:00:00 +0530

    User Authentication Done

---
+++ backend/src/controller/auth.controller.js
@@ -0,0 +1,87 @@
+const userModel = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+async function registerUser(req, res) {
+  try {
+    const { fullName, email, password } = req.body;
+    const userExists = await userModel.findOne({
+      email: email,
+    });
+    if (userExists) {
+      return res.status(400).json({
+        message: "User Already Exists",
+      });
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = await userModel.create({
+      fullName,
+      email,
+      password: hashPassword,
+    });
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.cookie("token", token);
+
+    res.status(201).json({
+      message: "User Created SuccessFully",
+      user: {
+        _id: user._id,
+        email: user.email,
+        fullName: user.fullName,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function loginUser(req, res) {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel
+      .findOne({
+        email,
+      })
+      .select("+password");
+    if (!user) {
+      return res.status(404).json({
+        message: "User Not Found",
+      });
+    }
+
+    const comparePassword = await bcrypt.compare(password, user.password);
+    if (!comparePassword) {
+      return res.status(400).json({
+        message: "Unauthorized User",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.cookie("token", token);
+
+    res.status(200).json({
+      message: "User LoggedIn Successfully",
+      user: {
+        _id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = { registerUser, loginUser };
