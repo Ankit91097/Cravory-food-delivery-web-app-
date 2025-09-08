6a9028c7b90b043893b10fbf7d08d239d630449f
Ankit Niranjan <ankitniranjan331@gmail.com>
2025-09-08 14:00:00 +0530

    Food Partner Authentication Done

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,6 +1,7 @@
 const userModel = require("../models/user.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const foodPartnerModel = require("../models/foodpartner.model");
 
 async function registerUser(req, res) {
   try {
@@ -91,4 +92,103 @@ async function logoutUser(req, res) {
   });
 }
 
-module.exports = { registerUser, loginUser, logoutUser };
+async function registerFoodPartner(req, res) {
+  try {
+    const { name, email, password } = req.body;
+    const foodPartnerExists = await foodPartnerModel.findOne({
+      email: email,
+    });
+    if (foodPartnerExists) {
+      return res.status(400).json({
+        message: "Food Partner Already Exists",
+      });
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const foodpartner = await foodPartnerModel.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+    const token = jwt.sign(
+      {
+        id: foodpartner._id,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.cookie("token", token);
+
+    res.status(201).json({
+      message: "Food Partner Created SuccessFully",
+      user: {
+        _id: foodpartner._id,
+        email: foodpartner.email,
+        name: foodpartner.name,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function loginFoodPartner(req, res) {
+  try {
+    const { email, password } = req.body;
+    const foodpartner = await foodPartnerModel
+      .findOne({
+        email,
+      })
+      .select("+password");
+    if (!foodpartner) {
+      return res.status(404).json({
+        message: "Food Partner Not Found",
+      });
+    }
+
+    const comparePassword = await bcrypt.compare(
+      password,
+      foodpartner.password
+    );
+    if (!comparePassword) {
+      return res.status(400).json({
+        message: "Unauthorized Food partner",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: foodpartner._id,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.cookie("token", token);
+
+    res.status(200).json({
+      message: "Food Partner LoggedIn Successfully",
+      foodpartner: {
+        _id: foodpartner._id,
+        fullName: foodpartner.fullName,
+        email: foodpartner.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function logoutFoodPartner(req, res) {
+  res.clearCookie("token");
+  res.status(200).json({
+    message: "Food Partner Logged Out Successfully",
+  });
+}
+
+module.exports = {
+  registerUser,
+  loginUser,
+  logoutUser,
+  registerFoodPartner,
+  loginFoodPartner,
+  logoutFoodPartner,
+};
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -8,4 +8,10 @@ router.post("/user/login", authController.loginUser);
 
 router.get("/user/logout", authController.logoutUser);
 
+router.post("/foodpartner/register", authController.registerFoodPartner);
+
+router.post("/foodpartner/login", authController.loginFoodPartner);
+
+router.get("/foodpartner/logout", authController.logoutFoodPartner);
+
 module.exports = router;
